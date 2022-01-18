db8b24a27f662a0b43178b300d99332ae25d4bfd
Miguel D Lopez <dimiguellopez@gmail.com>
2022-01-18 09:57:07 -0500

    modi 1801

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -56,7 +56,7 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-  var obj = {
+  let  obj = {
     nombre : nombre,
     email : email,
     password : password,
